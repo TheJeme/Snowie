dedfcaf0743b34fdc9a22a9f6715f6489ee53128
Jeme <eemelijoonatan@gmail.com>
2020-04-02 21:19:24 +0200

    UI

--- Snowie/MainWindow.xaml.cs
+++ Snowie/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Snowie
 {
@@ -20,9 +13,59 @@ namespace Snowie
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isConnecting;
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(700);
+            dt.Tick += dtTicker;
+            dt.Start();
+
+            isConnecting = false;
+        }
+
+        private void dtTicker(object sender, EventArgs e)
+        {
+            if (isConnecting)
+            {
+                if (ConnectingLabel.Content.Equals("Connecting..."))
+                    ConnectingLabel.Content = "Connecting";
+                else if (ConnectingLabel.Content.Equals("Connecting"))
+                    ConnectingLabel.Content = "Connecting.";
+                else if (ConnectingLabel.Content.Equals("Connecting."))
+                    ConnectingLabel.Content = "Connecting..";
+                else if (ConnectingLabel.Content.Equals("Connecting.."))
+                    ConnectingLabel.Content = "Connecting...";
+            }
+        }
+
+        private void OpenChatWindow()
+        {
+            var chat = new ChatWindow();
+            chat.Show();
+        }
+
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isConnecting)
+            {
+                isConnecting = true;
+                ConnectButton.Content = "Cancel";
+                InfoPanel.Visibility = Visibility.Collapsed;
+                ConnectingPanel.Visibility = Visibility.Visible;
+                OpenChatWindow();
+                this.Close();
+            }
+            else
+            {
+                isConnecting = false;
+                ConnectButton.Content = "Connect";
+                InfoPanel.Visibility = Visibility.Visible;
+                ConnectingPanel.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
